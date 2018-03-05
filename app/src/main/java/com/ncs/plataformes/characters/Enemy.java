a8bf5b198b2e0df7530f760da720472016b25dac
NicosFabro <nicosfabroc@gmail.com>
2018-03-05 16:51:55 +0100

    Initial commit

---
+++ app/src/main/java/com/ncs/plataformes/characters/Enemy.java
@@ -0,0 +1,10 @@
+package com.ncs.plataformes.characters;
+
+import com.ncs.plataformes.GameEngine;
+
+abstract public class Enemy extends Character {
+
+    public Enemy(GameEngine gameEngine, int x, int y) {
+        super(gameEngine, x, y);
+    }
+}
